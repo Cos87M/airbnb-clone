59ab24567a507186d4e66c9851bc7e2a7f0b1258
Cos87M <mihaiadriancosmin@yahoo.com>
2023-05-18 04:51:57 +0200

    tailwind working

---
+++ spec/requests/pages_spec.rb
@@ -0,0 +1,12 @@
+require "rails_helper"
+
+# Method to test rspec
+
+RSpec.describe "Pages", type: :request do
+  describe "GET /index" do
+    it "succeeds" do
+      get root_path
+      expect(response).to be_successful
+    end
+  end
+end
