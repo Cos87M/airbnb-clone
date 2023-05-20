6d4347e829c8762c44ff90d487a19a7ee68ab7b6
Cos87M <mihaiadriancosmin@yahoo.com>
2023-05-20 16:28:48 +0200

    add tailwind css for navbar

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,8 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root "pages#home"
+
+  get '/tailwindcss/base', to: redirect('/path/to/application.scss')
+  get '/tailwindcss/components', to: redirect('/path/to/components.css')
+  get '/tailwindcss/utilities', to: redirect('/path/to/utilities.css')
 end
