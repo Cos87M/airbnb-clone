6fbe30f66df93c52c34b2cf22f741a111fe494c0
Cos87M <mihaiadriancosmin@yahoo.com>
2023-06-01 18:36:46 +0200

    ad a close button to the modal

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -3,16 +3,22 @@ import { enter, leave, toggle } from "el-transition"
 
 // Connects to data-controller="controller-modal"
 export default class extends Controller {
+  static targets = ["closeButton"];
 
   connect() {
 
     document.getElementById('modal-wrapper').addEventListener('click', this.closeModal);
 
+    this.closeButtonTarget.addEventListener('click', () => {
+      leave(document.getElementById('modal-wrapper'));
+      leave(document.getElementById('modal-backdrop'));
+      leave(document.getElementById('modal-panel'));
+    });
   }
 
   closeModal(event) {
     const modalPanelClicked =document.getElementById('modal-panel').contains(event.target);
-    console.log(event.target.id);
+
 
     if(!modalPanelClicked && event.target.id !== "modal-trigger") {
       leave(document.getElementById('modal-wrapper'));
