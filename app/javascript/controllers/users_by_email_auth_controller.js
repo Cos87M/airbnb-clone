12e794ca1533b13b1d92cbc67efe0fd684209271
Cos87M <mihaiadriancosmin@yahoo.com>
2023-06-06 04:55:57 +0200

    Empty email form state error message

--- app/javascript/controllers/users_by_email_auth_controller.js
+++ app/javascript/controllers/users_by_email_auth_controller.js
@@ -3,7 +3,7 @@ import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="users-by-email-auth"
 export default class extends Controller {
-  static targets = ['email', 'submit']
+  static targets = ['email', 'submit','emailWrapper','invalidSvg','errorMessage']
 
   connect() {
     // console.log('axios: ', axios);
@@ -13,6 +13,10 @@ export default class extends Controller {
       if(this.emailTarget.value.length === 0) {
         console.log('inside if email field is empty')
         // email field is empty, so don't do anything
+        this.emailWrapperTarget.classList.add('invalid-input-text-field');
+        this.emailWrapperTarget.classList.remove('text-gray-700');
+        this.invalidSvgTarget.classList.remove('hidden');
+        this.errorMessageTarget.classList.remove('hidden');
       } else {
         // email field is filled, so do something
         axios.get('/users_by_email', {
