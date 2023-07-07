fdae197097a01fbb12ee0eea3eb53be904064f98
Cos87M <mihaiadriancosmin@yahoo.com>
2023-07-07 05:56:13 +0200

    set up the Favorite model and the API

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,4 +2,6 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   it { have_one(:profile).dependent(:destroy) }
+  it { have_many(:favorites).dependent(:destroy) }
+  it { have_many(:favorited_properties).through(:favorites).source(:property) }
 end
---
+++ spec/requests/favorites_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+RSpec.describe "Favorites", type: :request do
+
+  let(:headers) do
+    {"ACCEPT" => "application/json" }
+  end
+  let(:user) { create(:user) }
+  let(:property) { create(:property) }
+
+  before { sign_in user }
+
+  describe "POST create" do
+    let(:params) do
+      {
+        user_id: user.id,
+        property_id: property.id
+      }
+    end
+
+    it "creates a new favorite" do
+      expect {
+        post favorites_path, params: params, headers: headers
+      }.to change(Favorite, :count).by(1)
+      expect(response.status).to eq 201
+    end
+  end
+  describe "DELETE destroy" do
+
+    it "deletes a favorite" do
+      favorite = create(:favorite)
+      expect {
+        delete favorite_path(favorite), headers: headers
+      }.to change(Favorite, :count).by(-1)
+      expect(response.status).to eq 204
+    end
+  end
+end
