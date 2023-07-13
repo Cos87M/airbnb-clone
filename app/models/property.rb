8f1fe9e7e420a17010d071987f8465e7a1c7aebc
Cos87M <mihaiadriancosmin@yahoo.com>
2023-07-13 04:53:30 +0200

    build the feature to get available dates for properties

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Property < ApplicationRecord
   validates :name, :headline, :description, :city, :country, :address_1, presence: true
 
@@ -28,6 +30,14 @@ class Property < ApplicationRecord
     return if user.nil?
 
     favorited_users.include?(user)
+  end
+
+  def available_dates
+    next_reservation = reservations.future_reservations.first
+    date_format = "%b %e"
+    return Date.tomorrow.strftime(date_format)..Date.today.end_of_year.strftime(date_format) if next_reservation === nil
+
+    Date.tomorrow.strftime(date_format)..next_reservation.reservation_date.strftime(date_format)
   end
 
 end
