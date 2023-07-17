2d4137235338177a9ea1ba42025d83ca0d6533a6
Cos87M <mihaiadriancosmin@yahoo.com>
2023-07-17 18:06:15 +0200

    build the share_modal for properties

--- app/javascript/controllers/share_controller.js
+++ app/javascript/controllers/share_controller.js
@@ -1,13 +1,18 @@
 import { Controller } from "@hotwired/stimulus"
 
+
 // Connects to data-controller="share"
 export default class extends Controller {
   connect() {
+    document.getElementById('share-modal-trigger').addEventListener('click', () => {
+      document.getElementById('modal-container').classList.toggle('hidden');
+    });
   }
   share(e) {
     e.preventDefault();
-    // console.log('share is connected');
-
-    document.getElementById('share-modal-trigger').click();
+    const modalTrigger = document.getElementById('share-modal-trigger');
+    if (modalTrigger) {
+      modalTrigger.click();
+    }
   }
 }
