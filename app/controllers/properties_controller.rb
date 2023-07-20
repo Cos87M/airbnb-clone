8807e542b613ca25ef32c843797b013ab2d43445
Cos87M <mihaiadriancosmin@yahoo.com>
2023-07-20 06:24:49 +0200

    add the reviews section

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,5 +1,6 @@
 class PropertiesController < ApplicationController
   def show
     @property = Property.find(params[:id])
+    @reviews = Review.where(reviewable_id: @property.id, reviewable_type: "Property")
   end
 end
--- app/javascript/controllers/modal_property_controller.js
+++ app/javascript/controllers/modal_property_controller.js
@@ -3,7 +3,7 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="modal-property"
 export default class extends Controller {
   connect() {
-    console.log('modal-property connected')
+    // console.log('modal-property connected')
   }
   showDescription(e) {
     e.preventDefault();
