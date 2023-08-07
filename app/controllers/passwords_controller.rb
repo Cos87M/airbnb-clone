230f8ca7241fb5bd5d04f547b3fe9dc2ff3299a3
Cos87M <mihaiadriancosmin@yahoo.com>
2023-08-07 14:54:36 +0200

    update users passwords

---
+++ app/controllers/passwords_controller.rb
@@ -0,0 +1,23 @@
+class PasswordsController < ApplicationController
+  before_action :authenticate_user!
+
+  def show
+    @user = current_user
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(password_params)
+      flash[:notice] = "Your password was updated: '#{password_params[:password]}'"
+    end
+    bypass_sign_in(@user)
+    session[:active_link_id] = "password" # Store the updated link ID in the session
+    redirect_to profile_path(@user)
+  end
+
+  private
+
+  def password_params
+    params.require(:password).permit(:password)
+  end
+end
