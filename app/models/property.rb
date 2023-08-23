7e6918f1b614ae927cb9d3b0be993c8cfdf5d0ce
Cos87M <mihaiadriancosmin@yahoo.com>
2023-08-23 15:10:31 +0200

    Bundler fix

--- app/models/property.rb
+++ app/models/property.rb
@@ -7,7 +7,7 @@ class Property < ApplicationRecord
 
   geocoded_by :address
   after_validation :geocode, if: -> { latitude.blank? && longitude.blank? }
-  after_validation :geocode, if: :will_save_change_to_address?
+
 
   belongs_to :user
   has_many_attached :images, dependent: :destroy
